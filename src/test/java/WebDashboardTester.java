4071fded35b0f51b1392850a7a55daeb93ee4032
Andrew Sun <me@andrewsun.com>
2016-12-29 23:05:03 -0500

    Add small test application

--- src/test/java/WebDashboardTester.java
+++ src/test/java/WebDashboardTester.java
@@ -1,3 +1,5 @@
+import com.google.gson.JsonElement;
+import org.stormgears.WebDashboard.RecordListener;
 import org.stormgears.WebDashboard.WebDashboard;
 
 /**
@@ -7,6 +9,11 @@ public class WebDashboardTester {
 	public static void main(String[] args) throws Exception {
 		WebDashboard.init("localhost:6020");
 		System.out.println(WebDashboard.getDouble("motor"));
-		WebDashboard.subscribeRecord();
+		WebDashboard.subscribeRecord("motor", new RecordListener() {
+			@Override
+			public void recordChanged(String path, JsonElement data) {
+				System.out.println(data);
+			}
+		});
 	}
 }
